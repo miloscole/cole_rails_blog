8ba017fda849e7e4acf1ad6e60427c5bab765e82
miloscole <27miloscole@gmail.com>
2024-04-10 10:45:02 +0200

    Add articles route, add articles controller with crud actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def page_title(obj)
+    case action_name
+    when "new", "create"
+      "Create new #{obj.class.to_s.downcase}"
+    when "edit", "update"
+      "Editing #{obj.class.to_s.downcase}"
+    else
+      nil
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "welcome#home"
+
+  resources :articles
 end
